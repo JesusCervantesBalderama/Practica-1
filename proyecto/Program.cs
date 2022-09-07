566dff14ec5aa06e3d438e528d95beaedc413161
JesusCervantesBalderama <jose.jesus.cervantes.balderrama@uabc.edu.mx>
2022-09-06 20:03:49 -0700

    Agregar archivos de proyecto.

---
+++ proyecto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jose Jesus Cervantes Balderrama");
+            Console.WriteLine("23 Años");
+            Console.WriteLine("Mexicali, BC");
+            Console.ReadKey();
+        
+
+        }
+    }
+}
